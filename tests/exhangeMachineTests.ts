aa283f118b1a3586c20e8082399bdd510697b8ee
hugo.damasceno <esmasiadamente@gmail.com>
2022-06-18 16:14:42 -0300

    Faltava extensão

--- tests/exhangeMachineTests.ts
+++ tests/exhangeMachineTests.ts
@@ -1,29 +1,36 @@
 const { assert, expect } = require('chai')
 const { ethers } = require("hardhat");
-const sollunah = require("../contracts")
+const { sollunah, ExchangeMachine } = require("../contracts")
 
+describe("exchangeMachine tests", () => {
+  let sollunahInstance;
+  let exchangeMachineInstance;
+  before(async () => sollunahInstance = await sollunah.new())
+  before(async () => exchangeMachineInstance = await ExchangeMachine.new())
 
-describe("isExchangeMachineDeployed", () => {
-  it("Should return contract address", async () => {
-    const TokenFactory = await ethers.getContractFactory("exchangeMachine");
-    const TokenDeploy = await TokenFactory.deploy(1);
-    await TokenDeploy.deployed();
+  describe("isDeployed", () => {
+    it("Should return contract address", async () => {
+      const TokenFactory = await ethers.getContractFactory("exchangeMachineInstance");
+      const TokenDeploy = await TokenFactory.deploy(1);
+      await TokenDeploy.deployed();
 
-    const deployAddress = await TokenDeploy.address;
-  })
-});
+      const deployAddress = await TokenDeploy.address;
+    })
+  });
 
-describe("isBalanceWorth", () => {
+  describe("isBalanceWorth", () => {
 
-  it("Should return balance of some wallet", async () => {
-    const supply = 1000;
-    const [Owner] = await ethers.getSigners();
+    it("Should return balance of some wallet", async () => {
+      const supply = 1000;
+      const [Owner] = await ethers.getSigners();
 
-    const TokenFactory = await ethers.getContractFactory("exchangeMachine");
-    const TokenDeploy = await TokenFactory.deploy(supply);
-    await TokenDeploy.deployed();
+      const TokenFactory = await ethers.getContractFactory("exchangeMachine");
+      const TokenDeploy = await TokenFactory.deploy(supply);
+      await TokenDeploy.deployed();
 
-    expect(await TokenDeploy.balanceOf(Owner.address)).to.equal(supply);
+      expect(await TokenDeploy.balanceOf(Owner.address)).to.equal(supply);
 
-  })
+    })
+  });
+
 });
